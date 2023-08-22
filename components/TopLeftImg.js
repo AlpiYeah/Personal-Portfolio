76e5915cf507191b9127fd806c9e615e95bbffc6
Alp <alpersyuleyman2002@gmail.com>
2023-08-22 15:01:40 +0200

    styled image

--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -3,7 +3,12 @@ import Image from "next/image";
 const TopLeftImg = () => {
   return (
     <div>
-      <Image src="/top-left-img.png" width={400} height={400} />
+      <Image
+        src="/top-left-img.png"
+        width={400}
+        height={400}
+        className="absolute left-0 top-0 mix-blend-color-dodge"
+      />
     </div>
   );
 };
