95e3edca0dd49ca1346844d7b8443dfb7c7824dc
Alp <alpersyuleyman2002@gmail.com>
2023-08-22 15:05:31 +0200

    made image responsive

--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -5,9 +5,10 @@ const TopLeftImg = () => {
     <div>
       <Image
         src="/top-left-img.png"
+        alt="top left image"
         width={400}
         height={400}
-        className="absolute left-0 top-0 mix-blend-color-dodge"
+        className="absolute left-0 top-0 mix-blend-color-dodge z-10 w-[200px] xl:w-[400px] opacity-50"
       />
     </div>
   );
