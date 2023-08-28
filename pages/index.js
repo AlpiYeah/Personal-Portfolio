cb247bc03413381f27d995d2a882eb6b6e74b596
Alp <alpersyuleyman2002@gmail.com>
2023-08-28 18:13:59 +0200

    started on the homepage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,29 @@
+import Image from "next/image";
+import ParticlesContainer from "../components/ParticlesContainer";
+import ProjecstBtn from "../components/ProjectsBtn";
+import Avatar from "../components/Avatar";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="bg-primary/60 h-full">
+      <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/">
+        <div className="text-center felx flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
+          <h1 className="h1">
+            Transforming Ideas <br /> Into{" "}
+            <span className="text-accent">Digital Reality</span>
+          </h1>
+          <p className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+            pulvinar viverra viverra. Praesent tristique laoreet nisl et
+            posuere.
+          </p>
+        </div>
+      </div>
+      <div>image</div>
+    </div>
+  );
 };
 
 export default Home;
