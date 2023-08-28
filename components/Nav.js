12aa01f8f0001a2482b300404f6921bea73fc7d5
Alp <alpersyuleyman2002@gmail.com>
2023-08-28 15:55:22 +0200

    nav bar styling finished

--- components/Nav.js
+++ components/Nav.js
@@ -33,11 +33,19 @@ const Nav = () => {
   const router = useRouter();
   const pathname = router.pathname;
   return (
-    <nav>
+    <nav className="flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl_h-screen">
       {}
-      <div className="flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-80 bg-red-200/10 xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full">
+      <div className="flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[80px] bg-red-200/10 xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full">
         {navData.map((link, index) => {
-          return <Link href={link.path}>{link.icon}</Link>;
+          return (
+            <Link
+              className={`${link.path === pathname && "text-accent"}`}
+              href={link.path}
+              key={index}
+            >
+              <div>{link.icon}</div>
+            </Link>
+          );
         })}
       </div>
     </nav>
