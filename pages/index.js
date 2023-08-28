ad3d99d5e5c97e0cb13b22cd8fab0b20d9ebdfd7
Alp <alpersyuleyman2002@gmail.com>
2023-08-28 19:00:33 +0200

    added some filler text and animations to homepage

--- pages/index.js
+++ pages/index.js
@@ -10,18 +10,39 @@ const Home = () => {
     <div className="bg-primary/60 h-full">
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
         <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
-          <h1 className="h1">
+          <motion.h1
+            variants={fadeIn("down", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h1"
+          >
             Transforming Ideas <br /> Into{" "}
             <span className="text-accent">Digital Reality</span>
-          </h1>
-          <p className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">
+          </motion.h1>
+          <motion.p
+            variants={fadeIn("down", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
+          >
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
             pulvinar viverra viverra. Praesent tristique laoreet nisl et
             posuere.
-          </p>
+          </motion.p>
           <div className="flex justify-center xl:hidden relative">
             <ProjecstBtn />
           </div>
+          <motion.div
+            variants={fadeIn("down", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="hidden xl:flex"
+          >
+            <ProjecstBtn />
+          </motion.div>
         </div>
       </div>
       <div>image</div>
