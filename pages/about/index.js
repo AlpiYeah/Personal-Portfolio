8681e3ddd24343fa3a2ba00bce2a3a328266f902
Alp <alpersyuleyman2002@gmail.com>
2024-03-13 19:30:56 +0100

    completed about page

--- pages/about/index.js
+++ pages/about/index.js
@@ -66,7 +66,7 @@ const aboutData = [
         stage: "2010 - 2012",
       },
       {
-        title: "Intern - DEF Corporation",
+        title: "Intern - University of Bremen",
         stage: "2008 - 2010",
       },
     ],
@@ -104,17 +104,35 @@ const About = () => {
       >
         <Avatar />
       </motion.div>
-      <div className="mx-auto container h-full flex flex-col items-center xl:flex-row gap-x-2">
+      <div className=" container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
         <div className=" flex-1 flex flex-col justify-center ">
-          <h2 className="h2">
+          <motion.h2
+            variants={fadeIn("right", 0.2)}
+            initial="hidden"
+            exit="hidden"
+            animate="show"
+            className="h2"
+          >
             Captivating <span className=" text-accent">stories</span> birth
             magnificent designs.
-          </h2>
-          <p className=" max-w-[500px] mx-auto xl:mx-0 mb-6 xl:-mb-12 px-2 xl:px-0">
+          </motion.h2>
+          <motion.p
+            variants={fadeIn("right", 0.4)}
+            initial="hidden"
+            exit="hidden"
+            animate="show"
+            className=" max-w-[500px] mx-auto xl:mx-0 mb-6 xl:-mb-12 px-2 xl:px-0"
+          >
             Growing up in a small village in Bulgaria I was always fascinated by
             all Technology but never had the access to it.
-          </p>
-          <div>
+          </motion.p>
+          <motion.div
+            variants={fadeIn("right", 0.6)}
+            initial="hidden"
+            exit="hidden"
+            animate="show"
+            className=" hidden md:flex md:max-w-xl xl:max-w-none mx-auto xl:mx-0 mb-8"
+          >
             <div className=" flex flex-1 xl:gap-x-4">
               <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
                 <div className=" text-2xl xl:text-4xl font-extrabold text-accent mb-2">
@@ -141,9 +159,15 @@ const About = () => {
                 </div>
               </div>
             </div>
-          </div>
+          </motion.div>
         </div>
-        <div className=" flex flex-col w-full xl:max-w-[48%] h-[480px]">
+        <motion.div
+          variants={fadeIn("left", 0.4)}
+          initial="hidden"
+          exit="hidden"
+          animate="show"
+          className=" flex flex-col w-full xl:max-w-[48%] h-[480px]"
+        >
           <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
             {aboutData.map((item, itemIndex) => {
               return (
@@ -179,7 +203,7 @@ const About = () => {
               );
             })}
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
