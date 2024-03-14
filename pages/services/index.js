2921851fe54c984d5bb03089b8fa4fa089836226
Alp <alpersyuleyman2002@gmail.com>
2024-03-14 21:51:53 +0100

    added lightbulb animation to services page

--- pages/services/index.js
+++ pages/services/index.js
@@ -5,7 +5,13 @@ import { motion } from "framer-motion";
 import { fadeIn } from "../../variants";
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className=" h-full bg-primary/30 flex items-center">
+      <Circles />
+      <div className=" container mx-auto"></div>
+      <Bulb />
+    </div>
+  );
 };
 
 export default Services;
