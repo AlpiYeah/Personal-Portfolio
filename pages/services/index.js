f61d1f629ee306d13c5a0f2b3c5005f9f0353be5
Alp <alpersyuleyman2002@gmail.com>
2024-03-14 21:43:38 +0100

    setting up all necessary import for the service page

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,3 +1,9 @@
+import ServiceSlider from "../../components/ServiceSlider";
+import Bulb from "../../components/Bulb";
+import Circles from "../../components/Circles";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+
 const Services = () => {
   return <div>Services</div>;
 };
