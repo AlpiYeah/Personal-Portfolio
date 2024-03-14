582ccddd35d8f64443b77ce392510fa1dc5badf7
Alp <alpersyuleyman2002@gmail.com>
2024-03-14 23:05:38 +0100

    added some framer motion animations to everything

--- pages/services/index.js
+++ pages/services/index.js
@@ -11,17 +11,35 @@ const Services = () => {
       <div className=" container mx-auto">
         <div className="flex flex-col xl:flex-row gap-x-8">
           <div className=" text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
-            <h2 className="h2 xl:mt-8">
+            <motion.h2
+              variants={fadeIn("up", 0.3)}
+              animate="show"
+              initial="hidden"
+              exit="hidden"
+              className="h2 xl:mt-8"
+            >
               My Services<span className=" text-accent">.</span>
-            </h2>
-            <p className=" mb-4 max-w-[400px] mx-auto lg:mx-0">
+            </motion.h2>
+            <motion.p
+              variants={fadeIn("up", 0.4)}
+              animate="show"
+              initial="hidden"
+              exit="hidden"
+              className=" mb-4 max-w-[400px] mx-auto lg:mx-0"
+            >
               Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
               nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam.
-            </p>
+            </motion.p>
           </div>
-          <div className=" w-full xl:max-w-[65%]">
+          <motion.div
+            variants={fadeIn("down", 0.6)}
+            animate="show"
+            initial="hidden"
+            exit="hidden"
+            className=" w-full xl:max-w-[65%]"
+          >
             <ServiceSlider />
-          </div>
+          </motion.div>
         </div>
       </div>
       <Bulb />
