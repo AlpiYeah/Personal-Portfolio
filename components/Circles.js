d87573a7c2652bba06340927636add1140aaa832
Alp <alpersyuleyman2002@gmail.com>
2024-03-14 21:55:20 +0100

    adjusted both animations

--- components/Circles.js
+++ components/Circles.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Circles = () => {
   return (
-    <div className="w-[200px] xl:w-[300px] absolute -bottom-2 -right-16 mix-blend-color-dodge animate-[pulse_7s_infinite] duration-75 z-10">
+    <div className="w-[200px] xl:w-[300px] absolute -bottom-2 -right-16 mix-blend-color-dodge animate-[pulse_6s_infinite] duration-75 z-10">
       <Image
         src="/circles.png"
         width={260}
