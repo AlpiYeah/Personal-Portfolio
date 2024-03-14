45d5417cb2fbb5e2fd461f01634192337dc2ec5f
Alp <alpersyuleyman2002@gmail.com>
2024-03-14 22:05:19 +0100

    added some filler text

--- pages/services/index.js
+++ pages/services/index.js
@@ -8,7 +8,20 @@ const Services = () => {
   return (
     <div className=" h-full bg-primary/30 flex items-center">
       <Circles />
-      <div className=" container mx-auto"></div>
+      <div className=" container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-x-8">
+          <div className=" text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
+            <h2 className="h2 xl:mt-8">
+              My Services<span className=" text-accent">.</span>
+            </h2>
+            <p className=" mb-4 max-w-[400px] mx-auto lg:mx-0">
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam.
+            </p>
+          </div>
+          <ServiceSlider />
+        </div>
+      </div>
       <Bulb />
     </div>
   );
