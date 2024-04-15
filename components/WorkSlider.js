a41242a28b0c48b7de086441ed5521d6a44bae7d
Alp <alpersyuleyman2002@gmail.com>
2024-04-15 21:37:16 +0200

    added links

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -6,18 +6,26 @@ export const workSlider = {
         {
           title: "title",
           path: "/thumb1Cropped Cropped.png",
+          name: "ToDo List with LocalStorage",
+          link: "https://alpiyeah.github.io/ToDo-localStorage/",
         },
         {
           title: "title",
           path: "/thumb2 Cropped.png",
+          name: "PeliTube",
+          link: "https://pelitube.netlify.app/",
         },
         {
           title: "title",
           path: "/thumb3 Cropped.png",
+          name: "Frontend concept gaming website",
+          link: "https://alpiyeah.github.io/Gaming-website/",
         },
         {
           title: "title",
           path: "/thumb4 Cropped.png",
+          name: "Unique Portfolio Website",
+          link: "/",
         },
       ],
     },
@@ -39,6 +47,7 @@ import { Pagination } from "swiper";
 import { BsArrowRight } from "react-icons/bs";
 // next image
 import Image from "next/image";
+import Link from "next/link";
 
 const WorkSlider = () => {
   return (
@@ -56,32 +65,39 @@ const WorkSlider = () => {
             <div className="grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer">
               {slide.images.map((image, index) => {
                 return (
-                  <div
-                    className="relative rounded-lg overflow-hidden flex items-center justify-center group "
-                    key={index}
+                  <Link
+                    href={image.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
                   >
-                    <div className="flex items-center justify-center relative overflow-hidden group">
-                      {/* image */}
-                      <Image src={image.path} width={500} height={300} alt="" />
-                      {/* overlay gradient */}
-                      <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
-                      {/* title */}
-                      <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
-                        <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
-                          {/* title part 1 */}
-                          <div className="delay-100">LIVE</div>
-                          {/* title part 2 */}
-                          <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
-                            PROJECT
-                          </div>
-                          {/* icon */}
-                          <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200">
-                            <BsArrowRight />
+                    <div
+                      className="relative rounded-lg overflow-hidden flex items-center justify-center group "
+                      key={index}
+                    >
+                      <div className="flex items-center justify-center relative overflow-hidden group">
+                        {/* image */}
+                        <Image
+                          src={image.path}
+                          width={500}
+                          height={300}
+                          alt=""
+                        />
+                        {/* overlay gradient */}
+                        <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
+                        {/* title */}
+                        <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
+                          <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
+                            {/* title part 1 */}
+                            <div className="delay-100">{image.name}</div>
+                            {/* icon */}
+                            <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200">
+                              <BsArrowRight />
+                            </div>
                           </div>
                         </div>
                       </div>
                     </div>
-                  </div>
+                  </Link>
                 );
               })}
             </div>
